8461dfd91e89671c214e309b70a9bcccc59a3adb
Mario <mario@Marios-Mac-mini.local>
2026-01-30 15:25:16 +0100

    chore: remove unrelated testimonial display components

--- components/testimonials/index.ts
+++
@@ -1,4 +0,0 @@
-export { StarRating } from './StarRating'
-export { TestimonialForm } from './TestimonialForm'
-export { TestimonialCard } from './TestimonialCard'
-export { TestimonialCarousel } from './TestimonialCarousel'
